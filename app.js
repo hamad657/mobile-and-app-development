0678b8023edff710f75c4f70523c6c200c9105e2
Hajra Amin <143127660+hamad657@users.noreply.github.com>
2025-08-12 01:04:54 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,52 @@
+// loops  (three types of loop)
+// For loop
+// While loop
+// Do-while loop
+
+
+// var i;
+// for(i=0;i<=10;i++){
+//     document.write( i+ " ) hello world  <br> " )
+// }
+
+
+// var i;
+// for( i=10; i>=0; i-- ){
+//     document.write( i+ "<br>" )
+// }
+
+
+// var arr = [0,1,2,3,4,5,6,7,8,9,10,'hajra']
+// for(var i=0; i<=10; i++){
+//     document.write(arr[i] ,"<br>")
+// }
+
+
+// jb hmy array ki length pta nah ho 
+
+// var arr = [0,1,2,3,4,5, "hajra",6,7,8,9,10,'hajra']
+// for(var i=0; i<arr.length; i++){
+//     document.write(arr[i] ,"<br>")
+// }
+
+// How to print the table using loop
+
+//  for(var i=1; i<=10; i++){
+//    document.write(2+ "X" +i+ "=" +2*i ,"<br>")
+//   }
+
+
+// how to print table of random nbr to get userinp
+  
+//   var userinput = +prompt('enter value')
+//    for(var i=1; i<=10; i++){
+//     document.write(userinput+ "X" +i+ "=" +userinput*i ,"<br>")
+//   }
+
+
+
+//    var userinput = +prompt('enter value')
+//    var length = +prompt('enter length of table')
+//    for(var i=1; i<=length; i++){
+//     document.write(userinput+ "X" +i+ "=" +userinput*i ,"<br>")
+//   }
